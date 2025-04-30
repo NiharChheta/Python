e78be46d2096bf2154d3922ee6de51cc6390f6c1
Nihar Chheta <126687141+NiharChheta@users.noreply.github.com>
2025-04-30 15:22:44 +0200

    Add files via upload

---
+++ Programs/Dictionary.py
@@ -0,0 +1,12 @@
+#Dictionary
+Info = {
+    'Name': 'Nihar Chheta',
+    'Age': 29,
+    'mobile_No': 9974124734,
+    'City': 'Aarhus',
+    'Country': 'Denmark',
+    }
+print(Info)
+Info['Mail'] = 'niharchheta1609@gmail.com'
+print(Info)
+print(Info['Name'])
---
+++ Programs/Lists.py
@@ -0,0 +1,37 @@
+states = [
+    "Kerala",
+    "Rajasthan",
+    "Odisha",
+    "Manipur",
+    "Gujarat",
+    "Tamil Nadu",
+    "Bihar",
+    "Nagaland",
+    "Jharkhand",
+    "Telangana",
+    "Madhya Pradesh",
+    "Tripura",
+    "Punjab",
+    "Maharashtra",
+    "West Bengal",
+    "Uttar Pradesh",
+    "Sikkim",
+    "Chhattisgarh",
+    "Arunachal Pradesh",
+    "Goa",
+    "Karnataka",
+    "Assam",
+    "Haryana",
+    "Meghalaya",
+    "Mizoram",
+    "Andhra Pradesh",
+    "Himachal Pradesh",
+    "Uttarakhand"
+]
+print(states)
+#arrange list in ascending order 
+states.sort()
+print(states)
+#reverse list 
+states.reverse()
+print(states)
