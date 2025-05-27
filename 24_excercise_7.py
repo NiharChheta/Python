242dffc37f3d5cdc1b58edd9c42cf43ae2505d32
Nihar Chheta <niharchheta1609@gmail.com>
2025-05-27 14:25:40 +0200

    Excercise 7 Nested loop

--- 24_excercise_7.py
+++ 24_excercise_7.py
@@ -1,35 +1,32 @@
 row = 1
-while row <= 5:
-    # Print spaces
+while row <= 5:  # outer loop
     spaces = 5 - row
     while spaces > 0:
         print(" ", end=" ")
         spaces -= 1
 
-    # Print stars
     column = 1
     while column <= row:
         print("*", end=" ")
         column += 1
 
-    print()  # New line
+    print()
     row += 1
 
 print()
 
 row = 1
 while row <= 5:  # outer loop
-    # print spaces
+
     space = 5 - row
     while space > 0:
         print(" ", end=' ')
         space = space - 1
 
-    # print stars
     column = 1
     while column <= row:
         print("*", end='  ')  # extra space after * for triangle shape
         column = column + 1
 
-    print()  # new line
+    print()
     row = row + 1
